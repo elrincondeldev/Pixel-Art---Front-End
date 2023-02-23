2407186039ed6478e4f70d5d1edb43208afce2e7
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2023-02-23 22:29:31 +0100

    front test

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,73 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+const url = "http://13.38.113.187/grid/";
+const updateUrl = "http://13.38.113.187/color";
 
 function Game() {
+  const [timeLastUpdate, setTimeLastUpdate] = useState(0);
+  const [pixelSize, setPixelSize] = useState(10);
+  const [pixelCount, setpixelCount] = useState(64);
+  const [currentColor, setCurrentColor] = useState("#000000");
+  const canvasRef = useRef(null);
+
+  const updateGrid = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    fetch(url + timeLastUpdate)
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeLastUpdate(data.time_last_update);
+
+        data.tiles.forEach((tile) => {
+          ctx.beginPath();
+          ctx.fillStyle = tile?.tile_color ? `#${tile.tile_color}` : "#FFFFFF";
+          ctx.fillRect(
+            (tile.tile_id % pixelCount) * pixelSize,
+            ((tile.tile_id / pixelCount) >> 0) * pixelSize,
+            pixelSize,
+            pixelSize
+          );
+          ctx.stroke();
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      updateGrid();
+    }, 500);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleCanvasClick = (e) => {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const pixelX = Math.floor(x / pixelSize);
+    const pixelY = Math.floor(y / pixelSize);
+    const id = pixelY * pixelCount + pixelX;
+
+    const updatePixel = {
+      id,
+      color: currentColor.substr(1),
+    };
+    fetch(updateUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatePixel),
+    }).then((res) => {
+      if (res.ok) {
+        updateGrid();
+      }
+    });
+  };
+
   return (
     <>
       <header className="header">
@@ -8,9 +75,18 @@ function Game() {
       </header>
       <div className="main">
         <div className="settings">
-          <input type="color" id="colorPicker" value="#333333" />
+          <input
+            onInput={(e) => setCurrentColor(e.target.value)}
+            type="color"
+            id="colorPicker"
+          />
         </div>
-        <div id="grid" className="grid"></div>
+        <canvas
+          ref={canvasRef}
+          width="640"
+          height="640"
+          onClick={handleCanvasClick}
+        ></canvas>
       </div>
     </>
   );
